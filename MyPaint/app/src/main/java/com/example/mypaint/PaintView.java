9a5ecfd117752660f0fa3f674621ecf4df86c153
Batschauer <nathan01batschauer@gmail.com>
2021-07-07 23:27:38 -0300

    Projeto My Paint

---
+++ MyPaint/app/src/main/java/com/example/mypaint/PaintView.java
@@ -0,0 +1,62 @@
+package com.example.mypaint;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Point;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+public class PaintView extends View {
+    private Paint mPaint;
+    private List<Point> mPoints;
+    private Path mPath;
+
+    public PaintView(Context context, @Nullable AttributeSet attrs) {
+        super(context);
+
+        mPaint = new Paint();
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setColor(Color.BLUE);
+        mPaint.setTextSize(20);
+
+        mPath = new Path();
+    }
+
+    //@Override
+    protected void onDraw(Canvas canvas) {
+        //super.onDraw(canvas);
+
+        canvas.drawPath(mPath, mPaint);
+    }
+
+    //@Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float pointX = event.getX();
+        float pointY = event.getY();
+
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                mPath.moveTo(pointX, pointY);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                mPath.lineTo(pointX, pointY);
+                break;
+            default:
+                return false;
+        }
+        postInvalidate();
+        return true;
+    }
+
+    public void changeColorBrushTool(int color) {
+        mPaint.setColor(color);
+    }
+}
