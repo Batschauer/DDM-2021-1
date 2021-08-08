211256b15ecd98eead13acef7962d2b107bdc24a
Batschauer <nathan01batschauer@gmail.com>
2021-08-08 10:35:01 -0300

    Aplicação listar todas versões do android

--- Notepad/app/src/main/java/com/example/notepad/MainActivity.java
+++ Notepad/app/src/main/java/com/example/notepad/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.notepad;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private final String[] dados = new String[] {
+            "Cupcake", "Donut", "Eclair", "Froyo", "Gingerbread",
+            "Honeycomb", "Ice Cream Sandwich", "Jelly Bean",
+            "Kitkat", "Lollipop", "Marshmello", "Nougat",
+            "Oreo", "Pie"
+        };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listView_notas);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                dados);
+
+        listView.setAdapter(adapter);
     }
 }
